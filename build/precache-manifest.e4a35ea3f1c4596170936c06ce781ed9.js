829d08259d48a26ea150daae1ea5e96f39c979d3
Michael Brandt <mb16@hood.edu>
2019-10-02 20:43:19 -0400

    Added libraries for redux.

---
+++ build/precache-manifest.e4a35ea3f1c4596170936c06ce781ed9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bff6ce460bff4c961ee89fedde17276",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b871773c0d3859c5104f",
+    "url": "/static/css/main.bac88fd6.chunk.css"
+  },
+  {
+    "revision": "21aa479a25073a9338ce",
+    "url": "/static/js/2.9cee6335.chunk.js"
+  },
+  {
+    "revision": "b871773c0d3859c5104f",
+    "url": "/static/js/main.577268e7.chunk.js"
+  },
+  {
+    "revision": "d3d8f55f8f6cfdba0eae",
+    "url": "/static/js/runtime~main.c46be655.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
